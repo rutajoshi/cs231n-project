347fc80f187b33ef4a51775af7ee67017d1c8c9b
Ruta Joshi <ruta@Rutas-Wizarding-Manual.local>
2021-03-06 15:04:00 -0800

    Cutting videos works

--- mh_genQuestionJpgsAndJson.sh
+++ mh_genQuestionJpgsAndJson.sh
@@ -1,9 +1,13 @@
+RAW_VIDEO_DIR_PATH=/Users/ruta/stanford/pac/mentalhealth/local_phi_data/
 VIDEO_DIR_PATH=/Users/ruta/stanford/pac/mentalhealth/question_clipped/
 JPG_DIR_PATH=/Users/ruta/stanford/pac/mentalhealth/question_jpgs/
 CSV_DIR_PATH=/Users/ruta/stanford/pac/mentalhealth/mhq_local_labels/
 DEST_JSON_PATH=/Users/ruta/stanford/pac/mentalhealth/question_clipped/
 CROPS_DIR_PATH=/Users/ruta/stanford/pac/mentalhealth/question_cropsampled/
+ANN_PATH=/Users/ruta/stanford/pac/mentalhealth/local_annotations/
+CUT_TARGET_DIR=/Users/ruta/stanford/pac/mentalhealth/mhq_local_targets/
 
+python -m util_scripts.mh_cut_videos $RAW_VIDEO_DIR_PATH $CUT_TARGET_DIR $ANN_PATH mh
 #python -m util_scripts.mh_generate_question_video_jpgs $VIDEO_DIR_PATH $JPG_DIR_PATH mh
-python -m util_scripts.mh_crop_and_sample $JPG_DIR_PATH $CROPS_DIR_PATH mh
+#python -m util_scripts.mh_crop_and_sample $JPG_DIR_PATH $CROPS_DIR_PATH mh
 #python -m util_scripts.mh_json $CSV_DIR_PATH $JPG_DIR_PATH $DEST_JSON_PATH
---
+++ util_scripts/mh_cut_videos.py
@@ -0,0 +1,122 @@
+import subprocess
+import argparse
+from pathlib import Path
+import json
+
+from joblib import Parallel, delayed
+
+from subprocess import PIPE #added by Ruta
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+from moviepy.video.io.VideoFileClip import VideoFileClip
+
+def cut_video(video_file_path, dst_root_path, annotations_path):
+    # For the given video, pull the annotations from the annotation directory
+    # Go through the json and cut the video into 11 clips
+    # Store each clip in the dst_root_path directory under a folder named after the video
+    
+    name = video_file_path.stem
+    dst_dir_path = dst_root_path / name
+    dst_dir_path.mkdir(exist_ok=True)
+
+    video_num = int(name.split("_")[1])
+    annotations_file = "audio_only_" + str(video_num) + ".json"
+    f = open(str(annotations_path) + "/" + annotations_file,)
+    annotations = json.load(f)
+
+    if (len(annotations) < 23):
+        print("Only " + str(len(annotations)) + " objects in json file: " + str(annotations_file))
+        return
+
+    for i in range(11):
+        question = annotations[i*2]
+        answer = annotations[i*2 + 1]
+        assert(question["data"]["who"] == "interviewer")
+        assert(answer["data"]["who"] == "participant")
+        start_time = question["start"]
+        end_time = answer["end"]
+        question_num = answer["data"]["question"]
+        question_name = "q" + str(question_num) + ".mp4"
+        print("start = " + str(start_time) + "\nend = " + str(end_time))
+        print(question_name + " should be " + str(end_time - start_time) + " seconds long")
+        question_file_path = dst_dir_path / question_name
+        #ffmpeg_extract_subclip(video_file_path, start_time, end_time, targetname=question_file_path)
+
+        with VideoFileClip(str(video_file_path)) as video:
+            clip = video.subclip(start_time, end_time)
+            clip.write_videofile(str(question_file_path), audio_codec='aac')
+
+    f.close()
+    print("Finished cutting video: " + str(name))
+        
+def class_process(class_dir_path, dst_root_path, annotations_path):
+    if not class_dir_path.is_dir():
+        return
+
+    dst_class_path = dst_root_path / class_dir_path.name
+    dst_class_path.mkdir(exist_ok=True)
+
+    for video_file_path in sorted(class_dir_path.iterdir()):
+        cut_video(video_file_path, dst_class_path, annotations_path)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'dir_path', default=None, type=Path, help='Directory path of videos')
+    parser.add_argument(
+        'dst_path',
+        default=None,
+        type=Path,
+        help='Directory path of jpg videos')
+
+    parser.add_argument(
+        'ann_path',
+        default=None,
+        type=Path,
+        help='Directory path of json annotations for cutting')
+
+    parser.add_argument(
+        'dataset',
+        default='',
+        type=str,
+        help='Dataset name (mh | ntu | kinetics | mit | ucf101 | hmdb51 | activitynet)')
+    parser.add_argument(
+        '--n_jobs', default=-1, type=int, help='Number of parallel jobs')
+    parser.add_argument(
+        '--fps',
+        default=-1,
+        type=int,
+        help=('Frame rates of output videos. '
+              '-1 means original frame rates.'))
+    parser.add_argument(
+        '--size', default=240, type=int, help='Frame size of output videos.')
+    args = parser.parse_args()
+
+    if args.dataset in ['mh', 'kinetics', 'mit', 'activitynet']:
+        ext = '.mp4'
+    else:
+        ext = '.avi'
+
+    print("Hi there")
+
+    if args.dataset == 'activitynet':
+        video_file_paths = [x for x in sorted(args.dir_path.iterdir())]
+        status_list = Parallel(
+            n_jobs=args.n_jobs,
+            backend='threading')(delayed(video_process)(
+                video_file_path, args.dst_path, ext, args.fps, args.size)
+                                 for video_file_path in video_file_paths)
+    else:
+        class_dir_paths = [x for x in sorted(args.dir_path.iterdir())]
+        test_set_video_path = args.dir_path / 'test'
+        if test_set_video_path.exists():
+            class_dir_paths.append(test_set_video_path)
+
+        args.n_jobs = 1
+        print("Starting parallel jobs on: " + str(args.n_jobs) + " threads")
+
+        status_list = Parallel(
+            n_jobs=args.n_jobs,
+            backend='threading')(delayed(class_process)(
+                class_dir_path, args.dst_path, args.ann_path)
+                                 for class_dir_path in class_dir_paths)
