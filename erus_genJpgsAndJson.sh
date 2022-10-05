4dfc17f2d9a2030c0c92f409c4d1d52b8f91db4d
Ubuntu <ubuntu@ip-172-31-30-176.us-west-2.compute.internal>
2022-10-05 04:03:42 +0000

    Make tiny dataset json

--- erus_genJpgsAndJson.sh
+++ erus_genJpgsAndJson.sh
@@ -1,7 +1,12 @@
 VIDEO_DIR_PATH=/home/ubuntu/ruta/data/videos/
-JPG_DIR_PATH=/home/ubuntu/ruta/data/jpgs/
-CSV_DIR_PATH=/home/ubuntu/ruta/labels/split_labels/
-DEST_JSON_PATH=/home/ubuntu/ruta/labels/split_labels/
+#JPG_DIR_PATH=/home/ubuntu/ruta/data/jpgs/
+#CSV_DIR_PATH=/home/ubuntu/ruta/labels/split_labels/
+#DEST_JSON_PATH=/home/ubuntu/ruta/labels/split_labels/
+
+
+JPG_DIR_PATH=/home/ubuntu/ruta/data/tiny_jpgs/
+CSV_DIR_PATH=/home/ubuntu/ruta/labels/tiny_split_labels/
+DEST_JSON_PATH=/home/ubuntu/ruta/labels/tiny_split_labels/
 
 #python -m util_scripts.erus_generate_video_jpgs $VIDEO_DIR_PATH $JPG_DIR_PATH erus
 python -m util_scripts.erus_json $CSV_DIR_PATH $JPG_DIR_PATH $DEST_JSON_PATH
