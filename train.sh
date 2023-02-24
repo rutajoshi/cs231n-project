0d6b8e37be3180655c171c3f65d1b8692ac18d90
Ubuntu <ubuntu@ip-172-31-30-176.us-west-2.compute.internal>
2023-02-24 03:03:54 +0000

    latest state as of 2/23/23

--- train.sh
+++ train.sh
@@ -18,22 +18,45 @@
 #	--momentum 0.95 \
 #	--lr_scheduler multistep
 
+#CUDA_LAUNCH_BLOCKING=1
+#rm -rf /home/ubuntu/ruta/results/erus_binary
+#mkdir /home/ubuntu/ruta/results/erus_binary
+#python main.py --root_path /home/ubuntu/ruta/data \
+#        --video_path binary_jpgs \
+#        --annotation_path /home/ubuntu/ruta/labels/binary_labels/mh_01.json \
+#        --result_path ../results/erus_binary \
+#        --dataset erus \
+#        --n_classes 2 \
+#        --model resnet \
+#        --model_depth 101 \
+#        --batch_size 50 \
+#        --n_threads 4 \
+#        --checkpoint 30 \
+#        --n_epochs 200 \
+#        --learning_rate 1e-3 \
+#        --weight_decay 1e-3 \
+#        --momentum 0.95 \
+#        --lr_scheduler multistep
+
+
+####### TINY DATASET TRAINING #########
+
 CUDA_LAUNCH_BLOCKING=1
-rm -rf /home/ubuntu/ruta/results/erus_binary
-mkdir /home/ubuntu/ruta/results/erus_binary
+rm -rf /home/ubuntu/ruta/results/tiny_erus_multiclass
+mkdir /home/ubuntu/ruta/results/tiny_erus_multiclass
 python main.py --root_path /home/ubuntu/ruta/data \
-        --video_path binary_jpgs \
-        --annotation_path /home/ubuntu/ruta/labels/binary_labels/mh_01.json \
-        --result_path ../results/erus_binary \
-        --dataset erus \
-        --n_classes 2 \
-        --model resnet \
-        --model_depth 101 \
-        --batch_size 50 \
-        --n_threads 4 \
-        --checkpoint 30 \
-        --n_epochs 200 \
-        --learning_rate 1e-3 \
-        --weight_decay 1e-3 \
-        --momentum 0.95 \
-        --lr_scheduler multistep
+	--video_path tiny_jpgs \
+	--annotation_path /home/ubuntu/ruta/labels/tiny_split_labels/mh_01.json \
+	--result_path ../results/tiny_erus_multiclass \
+	--dataset erus \
+	--n_classes 4 \
+	--model resnet \
+	--model_depth 101 \
+	--batch_size 3 \
+	--n_threads 4 \
+	--checkpoint 100 \
+	--n_epochs 1000 \
+	--learning_rate 1e-2 \
+	--weight_decay 1e-3 \
+	--momentum 0.95 \
+	--lr_scheduler plateau
